9c22dc8ac2b6ad45fa604177843f2e8912cf23ce
Karthik Hosur <rheuzaki@gmail.com>
2023-11-07 15:16:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,109 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
+from supabase import create_client, Client
+import requests
+import json 
+import uuid
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+# Supabase settings
+SUPABASE_URL = "https://vqssuexxzajyciiqdnek.supabase.co"
+SUPABASE_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZxc3N1ZXh4emFqeWNpaXFkbmVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY1NjM2NTcsImV4cCI6MjAxMjEzOTY1N30.PdFq6mep2N8dU1vDXd6lofL8krKPjK8FA17lNYrm_N8"
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+# Define a dictionary to map page names to their corresponding content
+pages = {
+    "Home": "Welcome to the Home Page!",
+    "Containers": "Containers Management",
+    "Customers": "You can contact us here.",
+}
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+# Create a sidebar with navigation options
+selected_page = st.sidebar.radio("Navigation", list(pages.keys()), index=0)
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+# Display the content based on the selected page
+st.title(selected_page)
+st.write(pages[selected_page])
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
+data,count = supabase.table("webhook_listener").select('*').neq('processed','done').execute()
+# .eq('processed','new')
 
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+result = data[1]
+
+
+for r in result:
+    if 'method' not in r and 'body' in r and 'type' in r['body']:
+        if r['body']['type'] == 'payment.created':
+            payment_id = ""
+            customer_id = ""
+            container_id =""
+            merchant_id = r['body']['merchant_id']
+            if 'id' in r['body']['data']:
+                payment_id = r['body']['data']['id']
+            if  'customer_id' in r['body']['data']['object']['payment'] :
+                customer_id = r['body']['data']['object']['payment']['customer_id']
+            if 'note' in r['body']['data']['object']['payment']:
+                container_id = r['body']['data']['object']['payment']['note']
+            
+            supabase.table("container_transaction").insert({'merchant_id': merchant_id,'payment_id' : payment_id,
+                'customer_id' : customer_id,
+                'container_id' : container_id,
+                'status' : 'New'}).execute()
+            supabase.table('webhook_listener').update({'processed': 'done'}).eq('id',r['id']).execute()
+
+data1, count1 = supabase.table('webhook_listener').update({'processed': 'done'}).neq('processed','done').execute()
+if selected_page == "Containers":
+#     # View all the distributed containers 
+#     # Mark a container Recieved 
+#     # Or Mark a container Not Recieved
+    
+#     # Add a button to charge back for container not recieved
+#     # On creating charge back, change all status to Recieved
+#     # Get container id and store it in a table 
+    data,count = supabase.table("container_transaction").select('*').execute()
+    x = data[1]
+    st.table(x)
+
+    container_id = st.text_input("Enter the Container Number Returned:")
+    if st.button("Submit"):
+        supabase.table('container_transaction').update({'status': 'Returned'}).eq('container_id',container_id).execute()
+        st.write("Container Marked Returned")
+        st.experimental_rerun()
+    customer_id = st.text_input("Enter the Customer ID to be charged:")
+    
+    
+    if st.button("Charge Back"):
+        random_uuid = uuid.uuid4()
+
+        uuid_str = str(random_uuid)
+        url = "https://connect.squareupsandbox.com/v2/payments"
+
+        payload = json.dumps({
+        "amount_money": {
+            "currency": "USD",
+            "amount": 5
+        },
+        "idempotency_key":uuid_str,
+        "source_id": "ccof:customer-card-id-ok",
+        "accept_partial_authorization": True,
+        "autocomplete": False,
+        "customer_details": {
+            "customer_initiated": False,
+            "seller_keyed_in": True
+        },
+        "customer_id": customer_id,
+        })
+        headers = {
+        'Square-Version': '2023-10-18',
+        'Authorization': 'Bearer EAAAEI8s4HH7l_c3MaQQyKzs1pUzJBVh8Ecltg6jVpotispTElb1esm-zE2cqO5f',
+        'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        st.write("Customer was charged")
+        supabase.table('container_transaction').update({'status': 'Charged'}).eq('customer_id',customer_id).execute()
+
+# if selected_page == "Customers":
+#     # View the Customers Charged and the corresponding containers
+    
